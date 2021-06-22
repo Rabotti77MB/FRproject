44d043ad820892c42f5749326a3eac296a254fba
Alessandro Folloni <allefollo@gmail.com>
2021-06-22 10:41:03 +0200

    try background

--- src/main/java/FR/Aestethics/MainWindow.java
+++ src/main/java/FR/Aestethics/MainWindow.java
@@ -8,41 +8,39 @@ public class MainWindow extends JFrame {
     private final JButton Payments;
     private final JButton Magazzino;
     private final JButton Corrieri;
-    // private final JTextField Nome;
+    // private final JTextArea Nome;
 
     public MainWindow() {
         super("MBE: gestionale d'impresa");
-        // Nome=new JTextField("MBE");
+        // Nome=new JTextArea("MBE");
         Payments = new JButton("Gestione fatture");
-        // Payments.setAlignmentY(Component.CENTER_ALIGNMENT);
-        setResizable(true);
+        Payments.setBackground(Color.RED);
         Magazzino = new JButton("Gestione magazzino");
-        // Magazzino.setLayout(new FlowLayout(FlowLayout.CENTER));
+        Magazzino.setBackground(Color.GRAY);
         Corrieri = new JButton("Miglior corriere");
-        // Corrieri.setLayout(new FlowLayout(FlowLayout.RIGHT));
 
-        JPanel p1 = new JPanel(new GridLayout());
+        JPanel p1 = new JPanel();
+        // p1.getContentPane().add(new JPanelBackground("mbe.jpg"));
 
-        JPanel p2 = new JPanel(new GridLayout());
-        // p1.add(Nome);
-        p2.add(Payments, Component.CENTER_ALIGNMENT);
-        p1.add(p2);
+        // JPanel p2 = new JPanel(new GridLayout());
+        // p1.add(Nome,BorderLayout.NORTH);
+        p1.add(Payments);
         // p1.add(new JLabel("°C"));
         p1.add(Magazzino);
         p1.add(Corrieri);
 
         /** JPanel p2 = new JPanel(new GridLayout(1, 2));
-        p2.add(CFButton);
-        p2.add(FCButton);
+         p2.add(CFButton);
+         p2.add(FCButton);
 
-        JPanel p3 = new JPanel(new BorderLayout());
-        p3.add(p1, BorderLayout.CENTER);
-        p3.add(p2, BorderLayout.PAGE_END); */
+         JPanel p3 = new JPanel(new BorderLayout());
+         p3.add(p1, BorderLayout.CENTER);
+         p3.add(p2, BorderLayout.PAGE_END); */
 
-        /* JFrame methods called */
+        /** JFrame methods called */
         setContentPane(p1);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(1375, 1100);
+        setSize(375, 110);
         setVisible(true);
     }
 
